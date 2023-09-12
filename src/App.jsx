7ce0f3485eb4e96d448a5bd0530069f3fe61fd6d
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-09-12 10:33:52 -0600

    Feat: page layout and mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Layout from './Layout/Layout'
+import { data } from './mockData'
 
 function App() {
   
-  return (
-    <>
+  const [news, setNews] = useState(data.articles)
 
-    </>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout news={news} />}>
+
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Outlet, NavLink} from 'react-router-dom'
+// import { useState } from 'react'
+import './Layout.css'
+// import { data } from '../mockData'
+
+
+function Layout({news}) {
+
+
+  return (
+    <div>
+      <header>
+        <h1>Daily Dev</h1>
+        <div className='header-links'>
+          <NavLink>Top Stories</NavLink>
+          <NavLink>Sources</NavLink>
+          <NavLink>All News</NavLink>
+        </div>
+      </header>
+      <main>
+        <Outlet context={{news}} />
+      </main>
+    </div>
+  )
+}
+
+export default Layout
